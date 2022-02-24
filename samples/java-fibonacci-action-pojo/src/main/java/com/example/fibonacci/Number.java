61a3dbdb96763fb1c0dd0d9e2598276f85f1a347
Renato Cavalcanti <renato@cavalcanti.be>
2022-02-24 22:58:13 +0100

    annotated pojo

--- samples/java-fibonacci-action-pojo/src/main/java/com/example/fibonacci/Number.java
+++ samples/java-fibonacci-action-pojo/src/main/java/com/example/fibonacci/Number.java
@@ -1,9 +1,12 @@
 package com.example.fibonacci;
+ 
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 public class Number {
 
   public final long value;
 
+  @JsonCreator
   public Number(long value) {
     this.value = value;
   }
